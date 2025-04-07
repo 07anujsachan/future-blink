099f647517eaee2d83a4407f8f37b1f1a3006453
Anuj Sachan <anujsachan98@gmail.com>
2025-04-08 03:40:56 +0530

    update app.js

--- server/app.js
+++ server/app.js
@@ -19,7 +19,7 @@ app.use(cors());
 
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://future-blink-26r1.vercel.app/',
   methods: ['GET', 'POST', 'PUT', 'DELETE'], 
   credentials: true,
 }));
--- server/server.js
+++ server/server.js
@@ -4,19 +4,11 @@ const app = require("./app");
 const defineEmailJob = require("./jobs/emailJobs");
 const connectDB = require("./config/database");
 const agenda = require("./config/agenda");
-const cors = require('cors');
+
 
 const PORT = process.env.PORT || 5000;
 
 
-const corsOptions = {
-  origin: ['https://future-blink-26r1.vercel.app/'], 
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
-  credentials: true, 
-};
-
-
-app.use(cors(corsOptions));
 
 (async () => {
   await connectDB();
