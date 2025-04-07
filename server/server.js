07c2f851d7467e6e8d8719644ff17aeac60479c4
Anuj Sachan <anujsachan98@gmail.com>
2025-04-08 03:26:44 +0530

    add cors

--- server/server.js
+++ server/server.js
@@ -4,14 +4,14 @@ const app = require("./app");
 const defineEmailJob = require("./jobs/emailJobs");
 const connectDB = require("./config/database");
 const agenda = require("./config/agenda");
-
+const cors = require('cors');
 const PORT = process.env.PORT || 5000;
 
 (async () => {
   await connectDB();
   await agenda.start();
   defineEmailJob();
-
+  app.use(cors());
   app.listen(PORT, () => {
     console.log(`🚀 Server running at http://localhost:${PORT}`);
   });
