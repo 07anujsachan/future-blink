ea15a2e9c62b56c9dc30dce97bd6ba728a3f35d3
Anuj Sachan <anujsachan98@gmail.com>
2025-04-08 03:35:28 +0530

    add cors

--- server/server.js
+++ server/server.js
@@ -5,13 +5,24 @@ const defineEmailJob = require("./jobs/emailJobs");
 const connectDB = require("./config/database");
 const agenda = require("./config/agenda");
 const cors = require('cors');
+
 const PORT = process.env.PORT || 5000;
+
+
+const corsOptions = {
+  origin: ['https://future-blink-26r1.vercel.app/'], 
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+  credentials: true, 
+};
+
+
+app.use(cors(corsOptions));
 
 (async () => {
   await connectDB();
   await agenda.start();
   defineEmailJob();
-  app.use(cors());
+ 
   app.listen(PORT, () => {
     console.log(`🚀 Server running at http://localhost:${PORT}`);
   });
